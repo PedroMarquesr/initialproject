c6c1962067248ce636326f7bf504fc3f0acfa404
Pedro Marques <83501969+PedroMarquesr@users.noreply.github.com>
2024-08-01 11:29:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function togglemode() {
   const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "img/avatar-light.png");
+    img.setAttribute("src", "img/Avatar-light.png");
     alert("ok, vou mudar para modo claro!");
   } else {
     img.setAttribute("src", "img/Avatar.png");
